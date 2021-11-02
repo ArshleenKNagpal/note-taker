b770fd495b90dbc75244663e96935cb9c4a723f6
ArshleenKNagpal <90290536+ArshleenKNagpal@users.noreply.github.com>
2021-11-02 19:37:11 -0400

    updated delete

--- server.js
+++ server.js
@@ -20,19 +20,18 @@ app.use(express.static("public"));
 
 
 //route
-app.get("/notes", function (req, res) {
-res.sendFile(path.join(__dirname, "public/notes.html"));
+app.get("/", (req, res) => {
+  // "__dirname" is a global obj and gives you the path of the currently running file
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-//route
-app.get("/", function (req, res) {
-res.sendFile(path.join(__dirname, "public/index.html"));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-// API GET
-app.get("/api/notes", function (req, res) {
-console.log("/api/notes-get");
-res.json(db);
+// Read the db.json file and displays all the saved notes
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "db/db.json"));
 });
 
 // app.get("*", function (req, res) {
@@ -40,31 +39,65 @@ res.json(db);
 //   });
 
 
+// Display a single note
+app.get("/api/notes/:note", function (req, res) {
+  var noteSelect = req.params.note;
+  console.log(noteSelect);
+  res.json(noteSelect);
+});
 
 //API POST
 
+// app.post("/api/notes", (req, res) => {
+
+//   const { title, text } = req.body;
+  
+//   const newNote = {
+//   title,
+//   text,
+//   id: uuid.v1(),
+//   }
+//   fs.readFile("./db/db.json", (err, data) => {
+//   const parsedNotes = JSON.parse(data)
+//   parsedNotes.push(newNote)
+  
+//   fs.writeFile("./db/db.json", JSON.stringify(parsedNotes), (err) =>
+//   err
+//   ? console.log(err)
+//   : console.log("new Note Saved")
+//   )
+//   })
+//   res.sendFile(path.join(__dirname, "./db/db.json"))
+//   })
+
+
+
 app.post("/api/notes", (req, res) => {
+  // Should receive a new note to save on the request body
 
-  const { title, text } = req.body;
-  
-  const newNote = {
-  title,
-  text,
-  id: uuid.v1(),
-  }
-  fs.readFile("./db/db.json", (err, data) => {
-  const parsedNotes = JSON.parse(data)
-  parsedNotes.push(newNote)
-  
-  fs.writeFile("./db/db.json", JSON.stringify(parsedNotes), (err) =>
-  err
-  ? console.log(err)
-  : console.log("new Note Saved")
-  )
-  })
-  res.sendFile(path.join(__dirname, "./db/db.json"))
-  })
+  const addedNote = req.body;
+  // create a unique identifier with Date.now()
+  addedNote.id = Date.now();
+  // Add it to the db.json file
+  let noteData = fs.readFileSync("./db/db.json");
+  // Create new notes - takes in JSON input and parses the data
+  let noteTaker = JSON.parse(noteData);
+  // Push addedNote to array
+  noteTaker.push(req.body);
+  // Write and stringify new array
+  fs.writeFileSync("./db/db.json", JSON.stringify(noteTaker), (err, data) => {
+      if (err) throw err;
+      res.json(noteTaker);
+  });
+  // send the new added note/response back to the client
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
 
+
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/404.html'))
+);
 
 
 // Delete note
@@ -79,6 +112,12 @@ break;
 }
 }
 });
+
+
+
+
+
+
 
 
 //Write to db.json
